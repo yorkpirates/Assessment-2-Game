bcba3a8161fc7b55c0a58f8f839be24078719dc8
Ben Morrison <btm505@york.ac.uk>
2022-03-01 14:29:56 +0000

    Working buttons in Shop Screen
    Added ability to press the buttons within the shop screen.
    
    - [TEMP] Button to give player free money
    - Button to buy ammo
    - Button to continue to powerup screen
    
    Added new Powerup Screen where player can purchase power ups (actual power ups not yet implemented).
    
    Added ability to press ESC key to return through screen pages.

--- core/src/com/mygdx/game/Components/Pirate.java
+++ core/src/com/mygdx/game/Components/Pirate.java
@@ -12,10 +12,10 @@ import com.mygdx.utils.QueueFIFO;
  */
 public class Pirate extends Component {
     private int factionId;
-    private int plunder;
+    public static int plunder;
     protected boolean isAlive;
-    private int health;
-    private int ammo;
+    public static int health;
+    public static int ammo;
     private final int attackDmg;
 
     /**
@@ -44,7 +44,7 @@ public class Pirate extends Component {
         return plunder;
     }
 
-    public void addPlunder(int money) {
+    public static void addPlunder(int money) {
         plunder += money;
     }
 
@@ -80,10 +80,10 @@ public class Pirate extends Component {
     /**
      * Adds ammo
      *
-     * @param ammo amount to add
+     * @param newAmmo amount to add
      */
-    public void reload(int ammo) {
-        this.ammo += ammo;
+    public static void addAmmo(int newAmmo) {
+        ammo += newAmmo;
     }
 
     public int getHealth() {
