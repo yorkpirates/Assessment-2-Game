cdb9b32ec93912d8dbd81da699dcd44934fbdd00
ameliarwig <54843192+ameliarwig@users.noreply.github.com>
2022-04-28 21:50:55 +0100

    testies

---
+++ tests/src/io/team9/game/tests/utils/TileMapCellsTest.java
@@ -0,0 +1,29 @@
+package io.team9.game.tests.utils;
+
+import com.mygdx.utils.TileMapCells;
+import io.team9.game.tests.GdxTestRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(GdxTestRunner.class)
+public class TileMapCellsTest {
+
+    TileMapCells tilemap = new TileMapCells();
+    @Test
+    public void testObstacle() {
+        assertEquals("obstacle", tilemap.OBSTACLE, 61);
+    }
+
+    @Test
+    public void testPassable() {
+        assertEquals("passable", tilemap.PASSABLE, 97);
+    }
+
+    @Test
+    public void testObstacleCost() {
+        assertEquals( tilemap.OBSTACLE_COST, 100000f, 100000f);
+    }
+
+}
