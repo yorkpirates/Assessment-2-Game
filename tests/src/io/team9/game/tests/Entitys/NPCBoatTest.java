a30c3fc00c6b6216a3b5e5d279107de865d5d702
H <93993068+printincident@users.noreply.github.com>
2022-04-28 21:33:43 +0100

    Added Npc tests

---
+++ tests/src/io/team9/game/tests/Entitys/NPCBoatTest.java
@@ -0,0 +1,36 @@
+package io.team9.game.tests.Entitys;
+import com.mygdx.game.Components.AINavigation;
+import com.mygdx.game.Components.Pirate;
+import com.mygdx.game.Entitys.NPCShip;
+import com.mygdx.game.Entitys.Ship;
+import com.mygdx.game.Managers.GameManager;
+import com.mygdx.game.Managers.PhysicsManager;
+import com.mygdx.game.Physics.CollisionInfo;
+import io.team9.game.tests.GdxTestRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+import static org.junit.Assert.*;
+@RunWith(GdxTestRunner.class)
+public class NPCBoatTest {
+
+    @Test
+    public void targetingTest(){
+        PhysicsManager.Initialize();
+        GameManager.Initialize();
+        NPCShip ship = new NPCShip();
+        Ship target = new Ship();
+        target.setFaction(2);
+        ship.setFaction(1);
+
+        CollisionInfo info = new CollisionInfo();
+        info.a=target;
+        info.b = ship;
+        ship.EnterTrigger(info);
+
+        assertEquals("Should return the target",ship.getComponent(Pirate.class).getTarget(),target);
+        AINavigation nav = ship.getComponent(AINavigation.class);
+        
+    }
+}
