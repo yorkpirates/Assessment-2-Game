b2009293ba0c626a9588112ebdeb71cf11340e7e
H <93993068+printincident@users.noreply.github.com>
2022-04-29 19:02:45 +0100

    Update CannonBallCollegeTest.java

--- tests/src/io/team9/game/tests/Entitys/CannonBallCollegeTest.java
+++ tests/src/io/team9/game/tests/Entitys/CannonBallCollegeTest.java
@@ -5,12 +5,17 @@ import com.mygdx.game.Components.RigidBody;
 import com.mygdx.game.Components.Transform;
 import com.mygdx.game.Entitys.CannonBall;
 import com.mygdx.game.Entitys.CannonBallCollege;
+import com.mygdx.game.Entitys.College;
+import com.mygdx.game.Managers.EntityManager;
+import com.mygdx.game.Managers.GameManager;
 import com.mygdx.game.Managers.PhysicsManager;
+import com.mygdx.utils.Constants;
 import io.team9.game.tests.GdxTestRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(GdxTestRunner.class)
 public class CannonBallCollegeTest {
@@ -26,5 +31,27 @@ public class CannonBallCollegeTest {
         loc.x=0;
         loc.y=0;
         assertEquals(loc,ball.getComponent(RigidBody.class).getVelocity());
+    }
+    @Test
+    public void moveTest(){
+        PhysicsManager.Initialize();
+        GameManager.Initialize();
+        College college = new College(1);
+        CannonBallCollege ball = new CannonBallCollege();
+        Vector2 vec = new Vector2(0,0);
+        Vector2 target = new Vector2(100,100);
+        ball.getComponent(Transform.class).setPosition(vec);
+        System.out.println(ball.getComponent(Transform.class).getPosition());
+        ball.fire(vec,target,college);
+
+        Constants.INIT_CONSTANTS();
+        for(int i =0;i<100;i++){
+            PhysicsManager.update();
+            ball.getComponent(RigidBody.class).update();
+
+
+        }
+        assertTrue("Should have equal x and y",ball.getComponent(Transform.class).getPosition().x==ball.getComponent(Transform.class).getPosition().y);
+        assertTrue("Not at Start",ball.getComponent(Transform.class).getPosition() !=vec);
     }
 }
